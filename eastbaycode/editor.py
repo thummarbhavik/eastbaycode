ad4524a1314a4e2184376d95229f217c8e30119e
Nguyen Hien <hiennguyen83@gmail.com>
2018-08-28 11:56:09 -0700

    add question in html file

--- eastbaycode/editor.py
+++ eastbaycode/editor.py
@@ -1,14 +1,27 @@
 from flask import Flask, render_template, request
 
+
 app = Flask(__name__)
+
+example = {}
+
+example["Question"] = '''Given a string s, find the longest palindromic substring in s.
+                        You may assume that the maximum length of s is 1000.'''
+example['Example'] = [{'input': '"babad"', 'output': '"bab"', 'note': '"aba" is also a valid answer.'},
+                        {'input': '"cbbd"', 'output': '"bb"'}]
 
 @app.route("/", methods= ['GET', 'POST'])
 def displayTextEditor():
     code = ''
+    result = ''
+
     if request.method == 'POST':
         code = request.form['code']
+        result = runcode(code)
+    return render_template("text_editor.html", code=code, result=result, problem=example)
 
-    return render_template("text_editor.html", code=code)
+def runcode(code):
+    return "Your code result"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
