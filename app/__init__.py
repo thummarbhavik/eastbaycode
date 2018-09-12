3515ac5e1a463ce3d176e968478d608179549509
Nguyen Hien <hiennguyen83@gmail.com>
2018-09-12 13:16:50 -0700

    using SQLAlchemy, Migrate and Google Sign in

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
-from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_debugtoolbar import DebugToolbarExtension
+from config import Config
 
 app = Flask(__name__)
-config = Config()
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+login.session_protection = 'strong'
 
-from app import routes
+from app import routes, models
