a4f86b7feadd20a774b7100b1292c909fa8bdfa0
Hien Nguyen <hiennguyen83@gmail.com>
2018-09-17 16:10:07 -0700

    Hien (#45)
    * add the input in create problem page and show problems
    * add create course, example, testcase
    * change navbar
    * move gitignore to upper folder
    * fix error on profile
    * link to problem page
    * add profile.html file

--- webapp/app/routes.py
+++ webapp/app/routes.py
@@ -10,7 +10,7 @@ from app.forms import QuestionForm, TestCaseForm, ExamplesForm, CourseForm
 from app.login_google import get_google_auth
 from config import Config
 from requests.exceptions import HTTPError
-# from app.msgqueue import push_msg, get_result
+from app.msgqueue import push_msg, get_result
 
 @app.route('/')
 @login_required
@@ -96,7 +96,8 @@ def create_assignment():
 @login_required
 def profile():
     # show avatar, name, email address
-    pass
+    user = Users.query.filter_by(id=current_user._get_current_object().id).first()
+    return render_template('profile.html', user=user)
 
 @app.route('/login')
 def login():
@@ -160,13 +161,18 @@ def displayTextEditor(id):
     if request.method == 'POST':
         code = request.form['code']
         input_list=["bhavik"]
+
+        # result = runcode(code, inputs) - just for subprocess.run
+        # push msg to redis queue
+        msg = json.dumps({"code": code, "inputs": input_list,
+                          "prototype": "def sayHello(s)","handle": 38})
         push_msg(qname="work", msg=msg)
         time.sleep(10)
         result = get_result(qname="result")
         # inputs = problem.testcases.all()
         # return redirect(url_for(displayTextEditor))
     return render_template("text_editor.html", code=code, inputs=testcase,
-                            problem=problem,result = result)
+                            problem=problem, result = result)
 
 @app.route('/logout')
 @login_required
