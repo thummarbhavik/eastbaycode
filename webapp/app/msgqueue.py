4ea03c52eea14f5492523fa7424fa223c3dd3698
thummarbhavik <bhavikthummar@gmail.com>
2018-09-17 10:30:31 -0700

    Bhavik1 (#40)
    Proper end to end system with docker-compose

--- webapp/app/msgqueue.py
+++ webapp/app/msgqueue.py
@@ -1,6 +1,6 @@
 import redis
 
-con_redis = redis.Redis(host="localhost", port=6379, db=0)
+con_redis = redis.Redis(host="eastbaycode_redis_1", port=6379, db=0)
 
 # method to push the code into queue
 def push_msg(qname, msg):
@@ -10,6 +10,7 @@ def push_msg(qname, msg):
 # get the result from redis queue
 def get_result(qname):
     conn = con_redis
-    result = (conn.lpop(qname)).decode('utf-8')
+    result = conn.lpop(qname)
     if result != None:
+        result = result.decode('utf-8')
         return result
