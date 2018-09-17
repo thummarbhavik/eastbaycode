d641b3b7c952f5703d59d37607d5024cf39a296f
bhavik <bhavikthummar@gmail.com>
2018-09-16 17:05:12 -0700

    Propar end to end system with docker-compose

--- webapp/app/msgqueue.py
+++ webapp/app/msgqueue.py
@@ -10,6 +10,7 @@ def push_msg(qname, msg):
 # get the result from redis queue
 def get_result(qname):
     conn = con_redis
-    result = (conn.lpop(qname)).decode('utf-8')
+    result = conn.lpop(qname)
     if result != None:
+        result = result.decode('utf-8')
         return result
